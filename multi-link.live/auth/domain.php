63bb4b039dc9dfff38f5eba045da92d207ad8833
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-13 09:08:05 -0600

    error protocol

--- multi-link.live/auth/domain.php
+++ multi-link.live/auth/domain.php
@@ -1,5 +1,6 @@
 <?php
-$PROTOCOL = $_SERVER['SERVER_PROTOCOL'];
+$PROTOCOL = !isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off' ? 'http://' : 'https://';
 $PHP_DOMAIN = $PROTOCOL . $_SERVER['SERVER_NAME'];
 if ($PHP_DOMAIN == 'http://localhost') $PHP_DOMAIN .= ':' . $_SERVER['SERVER_PORT'];
+//$NODE_DOMAIN = 'http://localhost:3000';
 $NODE_DOMAIN = 'https://multi-link-api.herokuapp.com';
