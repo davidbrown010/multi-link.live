6de522b478ad5b37bd53b849a62c5fca531d8c94
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-13 08:07:54 -0600

    protocol bug fix

--- multi-link.live/auth/domain.php
+++ multi-link.live/auth/domain.php
@@ -1,5 +1,5 @@
 <?php
-
-$PHP_DOMAIN = $_SERVER['SERVER_NAME'];
-if ($PHP_DOMAIN == 'localhost') $PHP_DOMAIN .= ':' . $_SERVER['SERVER_PORT'];
+$PROTOCOL = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+$PHP_DOMAIN = $PROTOCOL . $_SERVER['SERVER_NAME'];
+if ($PHP_DOMAIN == 'http://localhost') $PHP_DOMAIN .= ':' . $_SERVER['SERVER_PORT'];
 $NODE_DOMAIN = 'localhost:3000';
