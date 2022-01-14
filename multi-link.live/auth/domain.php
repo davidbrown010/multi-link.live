fd1752dfa438d0a2208487f50c845b87e5292d10
David Brown <davidbrown@Davids-MacBook-Pro.local>
2022-01-13 22:09:24 -0600

    node domain change

--- multi-link.live/auth/domain.php
+++ multi-link.live/auth/domain.php
@@ -1,5 +1,5 @@
 <?php
 $PHP_DOMAIN = ($_SERVER['SERVER_NAME'] == 'localhost' ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'];
 if ($PHP_DOMAIN == 'http://localhost') $PHP_DOMAIN .= ':' . $_SERVER['SERVER_PORT'];
-$NODE_DOMAIN = 'http://localhost:3000';
-//$NODE_DOMAIN = 'https://multi-link-api.herokuapp.com';
+//$NODE_DOMAIN = 'http://localhost:3000';
+$NODE_DOMAIN = 'https://multi-link-api.herokuapp.com';
